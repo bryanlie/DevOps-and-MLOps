333d75bf85ad65897972d74303b950963636b22d
Dan Li <danli091981@gmail.com>
2024-04-06 21:41:59 -0400

    Update test.tf

--- Terraform/test.tf
+++ Terraform/test.tf
@@ -12,3 +12,13 @@ provider aws {
     region = "us-east-1"
 }
 
+# Create an EC2 instance
+resource "aws_instance" "main-server" {
+    ami = "ami-06e0ce9d3339cb039" 
+    instance_type = "t1.micro"
+    
+    tags = {
+        Name = "MainInstance"
+        System = "TerraformTutorial"
+    }
+}
